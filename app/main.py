d6290a0bf5eff3968dd8eaffc4385851f2ef0c88
Brent Berghmans <brent.berghmans@gmail.com>
2017-10-02 23:01:29 +0200

    Updated script, uses google storage api now, 404 if not found, adds index.html to urls without extension and fallbacks to original url if file not found, filters out query paramaters

--- app/main.py
+++ app/main.py
@@ -12,36 +12,114 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-import urllib2
 import webapp2
 import mimetypes
+import re
+import cloudstorage
+from urlparse import urlparse
+#import google.cloud
 
-def getContentType(filename, url):
-	contentType = mimetypes.MimeTypes().guess_type(filename)
-	return contentType
+def getContentType(filename):
+	if filename.lower().endswith('.css'):
+		return "text/css"
+	return mimetypes.MimeTypes().guess_type(filename)[0]
 
-class MainHandler(webapp2.RequestHandler):
+# https://stackoverflow.com/questions/8567171/how-do-i-remove-a-query-from-a-url
+def filterURL(filename):
+	o = urlparse(filename)
+	url_without_query_string = o.path[1:]
+	return url_without_query_string
+
+class ContentServerGAE(webapp2.RequestHandler):
+	BUCKET_NAME = "labo-cdn.appspot.com"
+
 	def get(self, filename):
-		# Get file from CDN
-		cdn = urllib2.urlopen("https://storage.googleapis.com/labo-cdn.appspot.com/" + filename)
-		contentType = getContentType(filename, cdn)[0]
+		# We add a 'valid' domain to be able to use this library, kind of hackish but it works
+		filename = filterURL("http://example.com/" + filename)
+		filename = "/" + self.BUCKET_NAME + "/" + filename
+		self.__hasExtenstionPattern = re.compile("^.+\.([a-zA-Z0-9])*$")
+		content = ""
+		contentType = getContentType(filename)
 
+		if not self.__hasExtenstionPattern.match(filename):
+			filenameIndexed = filename[:]
+			if not filenameIndexed.endswith("/"):
+				filenameIndexed = filenameIndexed + "/"
+
+			filenameIndexed = filenameIndexed + "index.html"
+			try:
+				with cloudstorage.open(filenameIndexed) as gcs_file:
+					content = content + gcs_file.read()
+
+				contentType = getContentType(filenameIndexed)
+				self.sendResponse(content, contentType)
+				return 
+
+			except cloudstorage.NotFoundError:
+				pass
+		
+		try:
+			with cloudstorage.open(filename) as gcs_file:
+				content = content + gcs_file.read()
+
+			self.sendResponse(content, contentType)
+
+		except cloudstorage.NotFoundError:
+			self.abort(404)
+			pass
+
+	def sendResponse(self, output, contentType):
 		self.response.headers['Content-Type'] = contentType
 		if contentType is "application/pdf":
 			self.response.headers['Content-Disposition'] = "inline"
 		
-		self.response.write(cdn.read())
+		self.response.write(output)
 
-class TestHandler(webapp2.RequestHandler):
+"""
+class ContentServerGC(webapp2.RequestHandler):
+	BUCKET_NAME = "labo-cdn.appspot.com"
+
 	def get(self, filename):
-		cdn = urllib2.urlopen("https://storage.googleapis.com/labo-cdn.appspot.com/" + filename)
-		contentType = getContentType(filename, cdn)[0]
+		self.__storageClient = google.cloud.storage.Client.from_service_account_json('content-server-auth.json')
+		self.__storageBucket = self.__storageClient.bucket(self.BUCKET_NAME)
+		self.__hasExtenstionPattern = re.compile("^.+\.([a-zA-Z0-9])*$")
 
-		self.response.headers['Content-Type'] = "text/plain"
-		self.response.write("You've entered: " + filename + "<br />" + contentType)
+		content = ""
+
+		contentType = getContentType(filename)[0]
+		blob = self.__storageBucket.blob(filename)
+
+		if not self.__hasExtenstionPattern.match(filename):
+			if not filename.endswith("/"):
+				filename = filename + "/"
+
+			filename = filename + "index.html"
+			indexedBlob = self.__storageBucket.blob(filename)
+
+			try:
+				content = indexedBlob.download_as_string()
+				contentType = getContentType(filename)[0]
+				self.sendResponse(content, contentType)
+
+			except google.cloud.exceptions.NotFound:
+				pass
+		
+		try:
+			content = blob.download_as_string()
+			self.sendResponse(content, contentType)
+
+		except google.cloud.exceptions.NotFound:
+			self.sendResponse("Not found", "text/plain")
+			pass
+
+	def sendResponse(output, contentType):
+		self.response.headers['Content-Type'] = contentType
+		if contentType is "application/pdf":
+			self.response.headers['Content-Disposition'] = "inline"
+		
+		self.response.write(output)
+"""
 
 app = webapp2.WSGIApplication([
-	webapp2.Route(r'/load/<:.*>', handler=MainHandler),
-	webapp2.Route(r'/test/<:.*>', handler=TestHandler)
+	webapp2.Route(r'/<:.*>', handler=ContentServerGAE),
 ], debug=True)
